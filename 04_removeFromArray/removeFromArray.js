8668eb515e98ad1c7cdd9a1e0afbf4faa63642a5
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2024-02-29 01:56:53 +0700

    Done 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...argu) {
+    for(let i = 0; i < argu.length; i++)
+    {
+        let index = array.indexOf(argu[i]);
+        while (index > -1){ 
+            array.splice(index, 1);
+            index = array.indexOf(argu[i]);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
