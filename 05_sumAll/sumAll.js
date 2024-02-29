90ac3ea8dca4552e9a8e3a7c0e8c97ce6ec7b402
MihnNathja <150262217+MihnNathja@users.noreply.github.com>
2024-02-29 11:18:59 +0700

    Done 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if( typeof a == "number" && typeof b == "number")
+    {
+        let start = a;
+        let end = b;
+        if (a >= b) 
+        {
+            start = b;
+            end = a;
+        }
+        if (start >= 0 && end >= 0)
+        {
+            let result = 0;
+            for (let i = start; i <= end ; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
